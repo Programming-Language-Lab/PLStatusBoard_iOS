02eef3cdda143a0d07db488e7a6c27752ba3d4c5
hooni <jhlee909090@gmail.com>
2023-05-24 19:38:03 +0900

    firestore 연동성공
    매핑연결
    firestore에 없는 멤버 추가하는 이슈 해결

--- PLAttendance/ViewController.swift
+++ PLAttendance/ViewController.swift
@@ -24,10 +24,11 @@ class ViewController: UIViewController {
 
 
 //
-   let imageNames = ["changwoo", "dojin",  "ockjihoon","goeun", "haesung", "hyunsoo", "juyeon"                      , "yijihoon", "yoojin"]
+   let imageNames = ["changwoo", "dojin", "goeun", "haesung", "hyunsoo",  "juyeon"                   , "ockjihoon"  , "yijihoon", "yoojin"]
 //    let memberNames = ["이창우", "김도진","옥지훈", "최고은", "이혜성", "전현수", "정주연" , "이지훈", "신유진"]
 //
-    
+    let nameToDocId = ["창우": "changwoo", "도진": "dojin", "옥지훈": "ockjihoon", "이ㅎㅅ": "haesung", "현수": "hyunsoo", "주연": "juyeon", "이지훈": "yijihoon", "유진": "yoojin", "고은": "goeun"]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -75,31 +76,53 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
     
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemberCollectionViewCell", for: indexPath) as! MemberCollectionViewCell
 
-        // 이미지 파일명은 이미지 이름 배열에서 가져옵니다.
-        let imageName = imageNames[indexPath.row]
-        cell.memberImage?.image = UIImage(named: imageName)
-        
         // 멤버 이름과 상태는 각각의 배열에서 가져옵니다.
         cell.mainLabel?.text = memberNames[indexPath.row]
-     //cell.statusLabel?.text = memberStatuses[indexPath.row]
+        //cell.statusLabel?.text = memberStatuses[indexPath.row]
         
+        // 이미지 파일명은 이미지 이름 배열에서 가져옵니다.
+        if indexPath.row < imageNames.count {
+            let imageName = imageNames[indexPath.row]
+            cell.memberImage?.image = UIImage(named: imageName)
+        }
+
         return cell
     }
 
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return memberNames.count // 배열 memberNames의 개수를 반환합니다.
-        
-        
     }
+
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemberCollectionViewCell", for: indexPath) as! MemberCollectionViewCell
+//
+//        // 이미지 파일명은 이미지 이름 배열에서 가져옵니다.
+//        let imageName = imageNames[indexPath.row]
+//        cell.memberImage?.image = UIImage(named: imageName)
+//
+//        // 멤버 이름과 상태는 각각의 배열에서 가져옵니다.
+//        cell.mainLabel?.text = memberNames[indexPath.row]
+//     //cell.statusLabel?.text = memberStatuses[indexPath.row]
+//
+//        return cell
+//    }
+//
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return memberNames.count // 배열 memberNames의 개수를 반환합니다.
+//
+//
+//    }
     
     func updateMemberStatus(at indexPath: IndexPath, status: String) {
         let memberName = memberNames[indexPath.row]
         let docRef = db.collection("Members").document(memberName)
-        
+        self.reloadMemberStatus(memberName: self.nameToDocId[self.memberNames[indexPath.row]] ?? "", newStatus: "재실")
+
         docRef.getDocument { (document, error) in
             if let document = document, document.exists {
                 docRef.updateData([
@@ -127,7 +150,7 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
     }
 
     
-    func updateMemberStatus(memberName: String, newStatus: String) {
+    func reloadMemberStatus(memberName: String, newStatus: String) {
         let docRef = db.collection("Members").document(memberName)
         
         // check if document exists
@@ -164,48 +187,44 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("Cell \(indexPath.row + 1) was tapped")
-        
+
         let alertController = UIAlertController(title: "멤버\(indexPath.row + 1)", message: "상태를 변경합니다.", preferredStyle: .actionSheet)
-        
+
         let inLab = UIAlertAction(title: "재실", style: .default) { (action) in
             print("멤버\(indexPath.row + 1)가 재실중입니다")
-            self.updateMemberStatus(memberName: self.memberNames[indexPath.row], newStatus: "재실")
-            
+            let docId = self.nameToDocId[self.memberNames[indexPath.row]] ?? ""
+            self.reloadMemberStatus(memberName: docId, newStatus: "재실")
         }
-        
+
         let goOut = UIAlertAction(title: "외출", style: .default) { (action) in
             print("멤버\(indexPath.row + 1)가 외출중입니다")
-            self.updateMemberStatus(memberName: self.memberNames[indexPath.row], newStatus: "외출")
-        
+            let docId = self.nameToDocId[self.memberNames[indexPath.row]] ?? ""
+            self.reloadMemberStatus(memberName: docId, newStatus: "외출")
         }
-        
+
         let inClass = UIAlertAction(title: "수업", style: .default) { (action) in
             print("멤버\(indexPath.row + 1)가 수업중입니다")
-            self.updateMemberStatus(memberName: self.memberNames[indexPath.row], newStatus: "수업")
-        
+            let docId = self.nameToDocId[self.memberNames[indexPath.row]] ?? ""
+            self.reloadMemberStatus(memberName: docId, newStatus: "수업")
         }
-        
+
         let goHome = UIAlertAction(title: "귀가", style: .default) { (action) in
             print("멤버\(indexPath.row + 1)가 집에 가버렷습니다!!!")
-            self.updateMemberStatus(memberName: self.memberNames[indexPath.row], newStatus: "귀가")
-        
-        
+            let docId = self.nameToDocId[self.memberNames[indexPath.row]] ?? ""
+            self.reloadMemberStatus(memberName: docId, newStatus: "귀가")
         }
-        
+
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        
+
         alertController.addAction(inLab)
         alertController.addAction(goOut)
         alertController.addAction(inClass)
         alertController.addAction(goHome)
         alertController.addAction(cancelAction)
-        
+
         self.present(alertController, animated: true, completion: nil)
-        
-        
     }
-    
-    
+
     
 }
 
